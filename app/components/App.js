1cb4a6deb16e3fd76d2d3a9400af5c823a80db9e
roscolil <ross.lillis@gmail.com>
2018-02-28 15:29:07 +1100

    attemting paginagtion

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import axios from 'axios'
+import InfiniteScroll from 'react-infinite-scroller'
+
 
 export default class App extends React.Component {
 
@@ -14,7 +16,8 @@ export default class App extends React.Component {
       operation: '',
       headers: {
         headers: {'user-key': '1e35415e3b9627ff3ad8ace342afde9c'}
-      }
+      },
+      resultOffset: 0
     }
   }
 
@@ -26,31 +29,48 @@ export default class App extends React.Component {
 
   clickSearch() {
     this.setState({
-      operation: 'Getting location..'
+      operation: 'Getting location...'
     })
 
     navigator.geolocation.getCurrentPosition((position) => {
-      var lat = position.coords.latitude
-      var lon = position.coords.longitude
-      var header = this.state.headers
-      var url =  `https://developers.zomato.com/api/v2.1/search?start=0&count=10&lat=${lat}&lon=${lon}&radius=5000&q=${this.state.cuisine}`;
+      let lat = position.coords.latitude
+      let lon = position.coords.longitude
+      let header = this.state.headers
+      let searchRadius = 5000
+      // let resultOffset = 0
+      let url =  `https://developers.zomato.com/api/v2.1/search?start=${this.state.resultOffset}&count=10&lat=${lat}&lon=${lon}&radius=5000&q=${this.state.cuisine}`;
+
 
       this.setState({
-        operation: 'Searching for fodder..'
+        operation: 'Searching for fodder...'
       })
 
       axios.get(url, header)
         .then(res => {
           this.setState({
             operation: 'Here are your results!',
-            nearbyRestaurants: res.data.restaurants
+            nearbyRestaurants: res.data.restaurants,
+            resultOffset: resultOffset + 10
           })
         })
+        // resultOffset += 10
+
+        // infiniteScroll() {
+        //   resultOffset += 10
+        //   let url2 = `https://developers.zomato.com/api/v2.1/search?start=${String(resultOffset)}&count=10&lat=${lat}&lon=${lon}&radius=${searsearchRadius}&q=${this.state.cuisine}`;
+        //   axios.get(url2, header)
+        //   .then(res => {
+        //     this.setState({
+        //       operation: 'Fetching more results..',
+        //       nearbyRestaurants: res.data.restaurants
+        //     })
+        //   })
+        // }
     })
   }
 
-  render() {
 
+  render() {
     const results = this.state.nearbyRestaurants
 
     return (
@@ -58,7 +78,7 @@ export default class App extends React.Component {
         <header>
           <div>
             <h1>Find my Fodder</h1>
-            <input type="text" placeholder="Cuisine" value={this.state.cuisine} onChange={this.inputChange}></input>
+            <input type="text" placeholder="Cuisine" id="cuisine-field" value={this.state.cuisine} onChange={this.inputChange}></input>
             <button  onClick={this.clickSearch}>Search</button>
             <br></br>
             <p>{this.state.operation}</p>
@@ -77,19 +97,19 @@ export default class App extends React.Component {
                     <img  src={item.thumb} />
                   </div>
                   <div className="details">
-                  <p>Name: <span>{item.name}</span></p>
-                  <p>Cuisine: <span>{item.cuisines}</span></p>
-                  <p>Rating: <span>{item.user_rating.aggregate_rating}</span></p>
-                  <p>Votes: <span>{item.user_rating.votes}</span></p>
-                  <p>Locality: <span>{item.location.locality}</span></p>
-                  <p>Address: <span>{item.location.address}</span></p>
-
+                    <p>Name: <span>{item.name}</span></p>
+                    <p>Cuisine: <span>{item.cuisines}</span></p>
+                    <p>Rating: <span>{item.user_rating.aggregate_rating}</span></p>
+                    <p>Votes: <span>{item.user_rating.votes}</span></p>
+                    <p>Locality: <span>{item.location.locality}</span></p>
+                    <p>Address: <span>{item.location.address}</span></p>
                   </div>
                 </div>
                   )
-              })}
+              })
+            }
             </div>
-
+            <button onClick={this.clickSearch}>More..</button>
         </main>
       </div>
     )
